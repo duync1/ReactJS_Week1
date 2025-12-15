fdf172b5575841e83a19916fb566b4b5b82a14c3
RSHCM2021\duync1 <duync1@runsystem.net>
2025-12-15 15:11:11 +0700

    First commit

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import importPlugin from "eslint-plugin-import";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  // Ignore
+  globalIgnores(["dist"]),
+
+  {
+    files: ["**/*.{ts,tsx}"],
+
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+
+    plugins: {
+      prettier,
+      import: importPlugin,
+    },
+
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: "./tsconfig.json",
+        },
+      },
+    },
+
+    rules: {
+      "prettier/prettier": "error",
+
+      "react/react-in-jsx-scope": "off",
+
+      "@typescript-eslint/no-unused-vars": ["warn"],
+
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal"],
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+            },
+          ],
+          "newlines-between": "always",
+        },
+      ],
+    },
+  },
+]);
