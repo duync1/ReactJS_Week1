0f0186837d837f829ebeae44e65220a3139e995f
RSHCM2021\duync1 <duync1@runsystem.net>
2026-01-05 16:55:34 +0700

    Update

--- src/redux/store/persist.ts
+++ src/redux/store/persist.ts
@@ -3,5 +3,5 @@ import storage from 'redux-persist/lib/storage'
 export const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['auth'], // chỉ lưu auth, không lưu products
+  whitelist: ['auth'],
 }
